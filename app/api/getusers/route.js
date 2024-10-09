3aa9111b21a55d117739484956290e25c69e763a
NawabMoazzam <cnstatus310@gmail.com>
2024-10-09 17:23:45 +0500

    updated

--- app/api/getusers/route.js
+++ app/api/getusers/route.js
@@ -2,26 +2,26 @@ import dbConnect from "@/lib/mongodb";
 import User from "@/models/User";
 import { NextResponse } from "next/server";
 
-// export async function GET() {
-//     // Connect to the database
-//     console.log('Connecting to database...');
-//     await dbConnect();
-//     console.log('Database connected, fetching users...');
-//     try {
-//         const users = await User.find({}).lean();
-//         if (!users || users.length === 0) {
-//             return NextResponse.json({ message: 'No Users Found' });
-//         }
-
-//         // Create the response object and set Cache-Control header
-//         const response = NextResponse.json(users);
-//         response.headers.set('Cache-Control', 'no-store');
-//         return response;
-//     } catch (error) {
-//         return NextResponse.json({ error: 'Error fetching users', details: error.message }, { status: 500 });
-//     }
-// }
-
 export async function GET() {
-    return NextResponse.json({ message: 'GET users API is working!' });
+    // Connect to the database
+    console.log('Connecting to database...');
+    await dbConnect();
+    console.log('Database connected, fetching users...');
+    try {
+        const users = await User.find({}).lean();
+        if (!users || users.length === 0) {
+            return NextResponse.json({ message: 'No Users Found' });
+        }
+
+        // Create the response object and set Cache-Control header
+        const response = NextResponse.json(users);
+        response.headers.set('Cache-Control', 'no-store');
+        return response;
+    } catch (error) {
+        return NextResponse.json({ error: 'Error fetching users', details: error.message }, { status: 500 });
+    }
 }
+
+// export async function GET() {
+//     return NextResponse.json({ message: 'GET users API is working!' });
+// }

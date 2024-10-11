61432edb27a381e31f3892fda83a925e34ecc5c3
NawabMoazzam <cnstatus310@gmail.com>
2024-10-11 11:01:12 +0500

    updated

--- app/components/header.js
+++ app/components/header.js
@@ -8,6 +8,7 @@ import { useState, useEffect } from 'react';
 const Header = () => {
     // Assuming username is stored in cookies
     const [username, setUsername] = useState('');
+    const [hideSignout, setHideSignout] = useState(true);
     const router = useRouter();
     const currentPath = usePathname();
 
@@ -61,14 +62,36 @@ const Header = () => {
                             <Link href={`/dashboard/${username}/utmlinks`} className="mr-5 hover:text-white">UTM-Links</Link>
                             <Link href={`/dashboard/${username}/utmgenerator`} className="mr-5 hover:text-white">UTM Generator</Link>
                         </nav>
-                        <button onClick={() => { handleLogout() }} className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base  md:mt-0">Log Out
+                        <button onClick={() => { setHideSignout(false) }} className="inline-flex items-center border-0 py-1 px-3 rounded text-base font-bold md:mt-0 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Log Out
                             <lord-icon
                                 src="https://cdn.lordicon.com/vduvxizq.json"
                                 trigger="hover"
-                                colors="primary:#9ca3af"
+                                colors="primary:#ffffff"
                                 style={{ "width": "25px", "height": "25px" }}>
                             </lord-icon>
                         </button>
+                        <div className={`bg-[#37415130] ${hideSignout ? "hidden" : "flex"} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full`}>
+                            <div className="h-screen flex items-center justify-center relative p-4 w-full max-w-md md:h-auto">
+                                <div className="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                                    <button onClick={() => { setHideSignout(true) }} className="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                        <span className="sr-only">Close modal</span>
+                                    </button>
+                                    <svg className="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2" />
+                                    </svg>
+                                    <p className="mb-4 text-gray-500 dark:text-gray-300">Are you sure you want to Log Out</p>
+                                    <div className="flex justify-center items-center space-x-4">
+                                        <button onClick={() => { setHideSignout(true) }} className="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                                            No, cancel
+                                        </button>
+                                        <button onClick={() => { handleLogout(); setHideSignout(true) }} className="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
+                                            Yes, I am sure
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </>
                 }
 

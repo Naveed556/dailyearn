d973ba1397a739ae0181af69183236f0464059c1
NawabMoazzam <cnstatus310@gmail.com>
2024-10-15 15:08:10 +0500

    Updated

--- app/components/header.js
+++ app/components/header.js
@@ -35,7 +35,7 @@ const Header = () => {
     return (
         <header className="text-gray-400 bg-gray-900 body-font">
             <div className="container mx-auto flex flex-wrap p-5 md:flex-row items-center justify-between">
-                <Link href={"http://localhost:3000/"} className="flex title-font font-medium items-center text-white md:mb-0">
+                <Link href={"/"} className="flex title-font font-medium items-center text-white md:mb-0">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
                         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                     </svg>

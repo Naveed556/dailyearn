2823c890a7bf72a331e84c287047a5e40cffc78f
NawabMoazzam <cnstatus310@gmail.com>
2024-10-26 12:17:57 +0500

    Updated

--- app/page.js
+++ app/page.js
@@ -266,10 +266,8 @@ export default function Home() {
               <h2 className="text-white text-4xl font-semibold">
                 Here are our heroes
               </h2>
-              <p className="text-lg leading-relaxed m-4 text-gray-300">
-                According to the National Oceanic and Atmospheric
-                Administration, Ted, Scambos, NSIDClead scentist, puts the
-                potentially record maximum.
+              <p className="text-xl leading-relaxed m-4 text-gray-300">
+                <span className="font-bold text-2xl text-gray-500">T</span>ogether <span className="font-bold text-2xl text-gray-500">E</span>veryone <span className="font-bold text-2xl text-gray-500">A</span>chieves <span className="font-bold text-2xl text-gray-500">M</span>ore!
               </p>
             </div>
           </div>
@@ -282,7 +280,7 @@ export default function Home() {
                   className="shadow-lg rounded-full max-w-full mx-auto"
                   style={{ maxWidth: "120px" }}
                 /> */}
-                <Image className="shadow-lg rounded-full max-w-full mx-auto" src={moazzam} width={120} alt="Nawab Moazzam" />
+                <Image className="shadow-lg rounded-full max-w-full mx-auto" src={moazzam} width={180} alt="Nawab Moazzam" />
                 <div className="pt-6 text-center">
                   <h5 className="text-white text-xl font-bold">
                     Nawab Moazzam
@@ -313,7 +311,7 @@ export default function Home() {
                   className="shadow-lg rounded-full max-w-full mx-auto"
                   style={{ maxWidth: "120px" }}
                 /> */}
-                <Image className="shadow-lg rounded-full max-w-full mx-auto" src={nabeel} width={120} alt="Nabeel Ameen" />
+                <Image className="shadow-lg rounded-full max-w-full mx-auto" src={nabeel} width={180} alt="Nabeel Ameen" />
                 <div className="pt-6 text-center">
                   <h5 className="text-white text-xl font-bold">
                     Nabeel Ameen
@@ -327,7 +325,7 @@ export default function Home() {
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
                       </svg>
                     </Link>
-                    <Link href={"#"} target="blank" className="w-8 h-8 m-1">
+                    <Link href={"https://www.instagram.com/nabeel.ahmad001/"} target="blank" className="w-8 h-8 m-1">
                       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="hover:text-pink-600 transition-all duration-500" viewBox="0 0 16 16">
                         <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
                       </svg>
@@ -344,7 +342,7 @@ export default function Home() {
                   className="shadow-lg rounded-full max-w-full mx-auto"
                   style={{ maxWidth: "120px" }}
                 /> */}
-                <Image className="shadow-lg rounded-full max-w-full mx-auto" src={aftab} width={120} alt="Aftab Ahmad" />
+                <Image className="shadow-lg rounded-full max-w-full mx-auto" src={aftab} width={180} alt="Aftab Ahmad" />
                 <div className="pt-6 text-center">
                   <h5 className="text-white text-xl font-bold">
                     Aftab Ahmad
@@ -353,12 +351,12 @@ export default function Home() {
                     Co-Founder
                   </p>
                   <div className="mt-3 flex items-center justify-center gap-4">
-                    <Link href={"#"} target="blank" className="w-8 h-8 m-1">
+                    <Link href={"https://www.facebook.com/brogrammer77/"} target="blank" className="w-8 h-8 m-1">
                       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="hover:text-blue-600 transition-all duration-500" viewBox="0 0 16 16">
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
                       </svg>
                     </Link>
-                    <Link href={"#"} target="blank" className="w-8 h-8 m-1">
+                    <Link href={"https://www.instagram.com/iffi.8/"} target="blank" className="w-8 h-8 m-1">
                       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="hover:text-pink-600 transition-all duration-500" viewBox="0 0 16 16">
                         <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
                       </svg>
@@ -375,7 +373,7 @@ export default function Home() {
                   className="shadow-lg rounded-full max-w-full mx-auto"
                   style={{ maxWidth: "120px" }}
                 /> */}
-                <Image className="shadow-lg rounded-full max-w-full mx-auto" src={naveed} width={120} alt="Naveed Ameen" />
+                <Image className="shadow-lg rounded-full max-w-full mx-auto" src={naveed} width={180} alt="Naveed Ameen" />
                 <div className="pt-6 text-center">
                   <h5 className="text-white text-xl font-bold">
                     Naveed Ameen
@@ -384,12 +382,12 @@ export default function Home() {
                     Founder and CEO
                   </p>
                   <div className="mt-3 flex items-center justify-center gap-4">
-                    <Link href={"#"} target="blank" className="w-8 h-8 m-1">
+                    <Link href={"https://www.facebook.com/profile.php?id=100052271644732"} target="blank" className="w-8 h-8 m-1">
                       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="hover:text-blue-600 transition-all duration-500" viewBox="0 0 16 16">
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
                       </svg>
                     </Link>
-                    <Link href={"#"} target="blank" className="w-8 h-8 m-1">
+                    <Link href={"https://www.instagram.com/naveednaveedali4483/"} target="blank" className="w-8 h-8 m-1">
                       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="hover:text-pink-600 transition-all duration-500" viewBox="0 0 16 16">
                         <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
                       </svg>

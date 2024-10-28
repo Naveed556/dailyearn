cca886d7b06ba8697ee2f32abbbb49706a98ac7c
NawabMoazzam <cnstatus310@gmail.com>
2024-10-28 19:27:17 +0500

    Updated

--- app/not-found.js
+++ app/not-found.js
@@ -6,9 +6,9 @@ export default function NotFound() {
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
             <div className="mx-auto max-w-screen-sm text-center">
                 <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-blue-500">Whoops!</h1>
-                <p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl text-white">Something's went wrong!</p>
+                <p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl text-white">Something went wrong!</p>
                 <p className="mb-4 text-lg font-light text-gray-400">
-                    Sorry, we can't find that page. Please <Link className="text-blue-500" href={"/"}>click here</Link> to go to Home Page.
+                    Sorry, we can not find that page. Please <Link className="text-blue-500" href={"/"}>click here</Link> to go to Home Page.
                 </p>
             </div>
         </div>

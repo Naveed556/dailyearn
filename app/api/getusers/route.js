ed5e1aebe11894a6d8bcadbf3060b52cd9707629
NawabMoazzam <cnstatus310@gmail.com>
2024-11-09 23:10:09 +0500

    Updated

--- app/api/getusers/route.js
+++ app/api/getusers/route.js
@@ -2,19 +2,41 @@ import dbConnect from "@/lib/mongodb";
 import User from "@/models/User";
 import { NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
+import jwt from "jsonwebtoken";
 
-export async function GET() {
-    revalidateTag("users")
-    try {
-        await dbConnect();
-        const users = await User.find({}).lean();
+export async function GET(request) {
+  try {
+    revalidateTag("users");
+    // Get the JWT token from cookies
+    const token = request.cookies.get("admintoken")?.value;
 
-        if (!users || users.length === 0) {
-            return NextResponse.json({ message: 'No Users Found' }, { status: 401 });
-        }
-
-        return NextResponse.json(users);
-    } catch (error) {
-        return NextResponse.json({ message: `Error fetching users: ${error.message}` }, { status: 500 });
+    if (!token) {
+      return NextResponse.json(
+        { message: "Unauthorized access" },
+        { status: 401 }
+      );
     }
+
+    // Verify the JWT token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    await dbConnect();
+    const users = await User.find({}).lean();
+
+    if (!users || users.length === 0) {
+      return NextResponse.json({ message: "No Users Found" }, { status: 401 });
+    }
+
+    return NextResponse.json(users);
+  } catch (error) {
+    if (error.name === "JsonWebTokenError") {
+      return NextResponse.json({ message: "Invalid token" }, { status: 401 });
+    } else if (error.name === "TokenExpiredError") {
+      return NextResponse.json({ message: "Token expired" }, { status: 401 });
+    } else {
+      return NextResponse.json(
+        { message: `Error fetching users: ${error.message}` },
+        { status: 500 }
+      );
+    }
+  }
 }

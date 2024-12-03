e955018f544cb780a9b440bf6cb0e4f3b4e1a676
NawabMoazzam <cnstatus310@gmail.com>
2024-12-03 14:26:21 +0500

    Added Warning Feature in Admin Panel

--- models/User.js
+++ models/User.js
@@ -1,37 +1,40 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const paymentsSchema = new mongoose.Schema({
-    dateRange: String,
-    month: String,
-    revenue: Number,
-    isPaid: Boolean
+  dateRange: String,
+  month: String,
+  revenue: Number,
+  isPaid: Boolean,
 });
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     commission: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
+    enableWarning: Boolean,
+    warningMessage: String,
     currentRevenue: Number,
     otp: String,
     otpExpires: Number,
-    payments: [paymentsSchema]
-},
-    { timestamps: true }
+    payments: [paymentsSchema],
+  },
+  { timestamps: true }
 );
 
-export default mongoose.models.User || mongoose.model('User', UserSchema);
+export default mongoose.models.User || mongoose.model("User", UserSchema);

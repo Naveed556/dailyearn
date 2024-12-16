2806da8be9e9a4693e2b27d1b62774c53d4a0207
NawabMoazzam <cnstatus310@gmail.com>
2024-12-16 23:41:19 +0500

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const AdminSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
         unique: true
     },
     username: {
